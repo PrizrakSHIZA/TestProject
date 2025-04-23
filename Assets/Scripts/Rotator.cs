7097203fe934bb3ca0f7f3e2e029414fccb01850
Prizrak <Prizrakshiza@gmail.com>
2025-04-23 21:09:54 +0300

    Rotation for Axe and Shuriken added

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 rotation;
+    Collider collider;
+
+    private void Start()
     {
-        
+        collider = GetComponent<Collider>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(collider.enabled)
+            transform.Rotate(rotation);
     }
 }
